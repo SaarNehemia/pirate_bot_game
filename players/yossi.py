c4f7f3de5a79dc606136107990e0bd3bf4842147
saar.nehemia <saar.nehemia@gmail.com>
2024-05-11 01:58:54 +0300

    rewrite player codes using only api

--- players/yossi.py
+++ players/yossi.py
@@ -2,58 +2,88 @@ import classes.api as game_api
 
 
 def do_turn(game_api: game_api.API):
-
-    strategy = 1
-
-    my_player_obj = game_api.get_my_player_obj()
-    if my_player_obj.ships:
-        my_first_ship = my_player_obj.ships[0]
-    else:
-        return
+    strategy = 3
+    my_player_id = game_api.get_my_player_id()
+    enemy_player_id = game_api.get_enemy_player_id()
 
     if strategy == 1:
         # Strategy 1: move my first ship to first enemy ship
-        enemy_player_id = ~my_player_obj.player_id
-        enemy_player = game_api.players[enemy_player_id]
-        if enemy_player.ships:
-            enemy_first_ship = enemy_player.ships[0]
-            game_api.move_ship_towards_location(ship=my_first_ship, location=enemy_first_ship.location)
+        try:
+            my_ships_ids = game_api.get_player_ships_ids(my_player_id)
+            my_first_ship_id = my_ships_ids[0]
+        except IndexError:
+            print('No ships for me :(')
+            return
+
+        try:
+            enemy_ships_ids = game_api.get_player_ships_ids(enemy_player_id)
+            first_enemy_ship_id = enemy_ships_ids[0]
+        except IndexError:
+            print('No ships for enemy :)')
+
+        enemy_ship_location = game_api.get_player_ship_location_from_id(player_id=enemy_player_id,
+                                                                        ship_id=first_enemy_ship_id)
+        game_api.move_ship_towards_location(ship_id=my_first_ship_id,
+                                            location=enemy_ship_location)
 
     elif strategy == 2:
-        # Strategy 2: move my first ship towards enemy base island
-        enemy_player_id = ~my_player_obj.player_id
-        enemy_base_island_location = game_api.islands[game_api.players_base_islands_indices[enemy_player_id]].location
-        game_api.move_ship_towards_location(ship=my_first_ship, location=enemy_base_island_location)
+        # Strategy 2: move my first ship towards first enemy island
+        try:
+            my_ships_ids = game_api.get_player_ships_ids(my_player_id)
+            my_first_ship_id = my_ships_ids[0]
+        except IndexError:
+            print('No ships for me :(')
+            return
+
+        try:
+            enemy_island_locations = game_api.get_player_owned_islands_locations(player_id=enemy_player_id)
+            first_enemy_island_location = enemy_island_locations[0]
+        except IndexError:
+            print('No enemy islands :)')
+            return
+
+        game_api.move_ship_towards_location(ship_id=my_first_ship_id,
+                                            location=first_enemy_island_location)
 
     elif strategy == 3:
-        # Strategy 3: move my first ship around my base island
+        # Strategy 3: move my first ship around my first island
         round_time = 300
-        my_base_island_location = game_api.islands[game_api.players_base_islands_indices[
-            game_api.get_my_player_id()]].location
-        if my_first_ship.location != my_base_island_location or game_api.num_turn == 1:
-            if 0 <= game_api.num_turn % round_time < 0.25 * round_time:
-                game_api.move_ship(ship=my_first_ship, direction='S')
-                return
-            elif 0.25 * round_time <= game_api.num_turn % round_time < 0.5 * round_time:
-                game_api.move_ship(ship=my_first_ship, direction='W')
-                return
-            elif 0.5 * round_time <= game_api.num_turn % round_time < 0.75 * round_time:
-                game_api.move_ship(ship=my_first_ship, direction='N')
-                return
-            elif 0.75 * round_time <= game_api.num_turn % round_time < round_time:
-                game_api.move_ship(ship=my_first_ship, direction='E')
-                return
+        rest_time = 50
 
-        if game_api.num_turn % 100 == 0:
-            if 0 <= game_api.num_turn % round_time < 0.25 * round_time:
-                game_api.move_ship(ship=my_first_ship, direction='S')
-                return
-            elif 0.25 * round_time <= game_api.num_turn % round_time < 0.5 * round_time:
-                game_api.move_ship(ship=my_first_ship, direction='W')
-                return
-            elif 0.5 * round_time <= game_api.num_turn % round_time < 0.75 * round_time:
-                game_api.move_ship(ship=my_first_ship, direction='N')
-                return
-            elif 0.75 * round_time <= game_api.num_turn % round_time < round_time:
-                game_api.move_ship(ship=my_first_ship, direction='E')
-                return
+        try:
+            my_ships_id = game_api.get_player_ships_ids(my_player_id)
+            my_first_ship_id = my_ships_id[0]
+
+            my_ships_locations = game_api.get_player_ships_locations(player_id=my_player_id)
+            my_first_ship_location = my_ships_locations[0]
+        except IndexError:
+            print('No ships for me :(')
+            return
+
+        try:
+            my_islands_locations = game_api.get_player_owned_islands_locations(player_id=my_player_id)
+            my_first_island_location = my_islands_locations[0]
+        except IndexError:
+            print('No islands for me :(')
+            return
+
+        # ship rounds island only if it is not in base island
+        if game_api.num_turn < game_api.get_num_players() or my_first_ship_location != my_first_island_location:
+            ship_go_round(game_api=game_api, ship_id=my_first_ship_id, round_time=round_time)
+            return
+
+        # ship leaves island if rest time passed
+        if game_api.num_turn % (round_time + rest_time) == 0:
+            ship_go_round(game_api=game_api, ship_id=my_first_ship_id, round_time=round_time)
+            return
+
+
+def ship_go_round(game_api: game_api.API, ship_id: int, round_time: int):
+    if 0 <= game_api.num_turn % round_time < 0.25 * round_time:
+        game_api.move_ship(ship_id=ship_id, direction='S')
+    elif 0.25 * round_time <= game_api.num_turn % round_time < 0.5 * round_time:
+        game_api.move_ship(ship_id=ship_id, direction='W')
+    elif 0.5 * round_time <= game_api.num_turn % round_time < 0.75 * round_time:
+        game_api.move_ship(ship_id=ship_id, direction='N')
+    elif 0.75 * round_time <= game_api.num_turn % round_time < round_time:
+        game_api.move_ship(ship_id=ship_id, direction='E')
